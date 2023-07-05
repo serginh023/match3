e54f8813ad9c04b29e60ed5637358fdfd7472582
Paulo Sergio da Silva Farias <psergio.farias@gmail.com>
2023-07-05 16:28:04 -0400

    Updated unity version to 2022.2.11

--- Assets/Scripts/Editor/GameControllerEditor.cs
+++ Assets/Scripts/Editor/GameControllerEditor.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.Experimental.TerrainAPI;
 
 [CustomEditor(typeof(GameController))]
 public class GameControllerEditor : Editor
