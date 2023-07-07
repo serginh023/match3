d2a80f9ace20da95648145a594c8e7c04bb3b06e
Paulo Sergio da Silva Farias <psergio.farias@gmail.com>
2023-07-07 13:25:03 -0400

    Add wall

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [SerializeField] private int collumns;
+    [SerializeField] private int rows;
+    [SerializeField] private List<ButtonCandy> buttonList;
+
+    
+}
