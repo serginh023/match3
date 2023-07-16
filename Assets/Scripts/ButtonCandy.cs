e5334280842680c32a995b6a80eb7da23efdaa6f
Paulo Sergio da Silva Farias <psergio.farias@gmail.com>
2023-07-16 19:32:40 -0400

    Add ButtonCandy prefab

--- Assets/Scripts/ButtonCandy.cs
+++ Assets/Scripts/ButtonCandy.cs
@@ -4,4 +4,28 @@ public class ButtonCandy : MonoBehaviour
 {
     [SerializeField] private Sprite icon;
     [SerializeField] private AudioClip audio;
+    [SerializeField] private GameObject hover;
+
+    private void OnClick()
+    {
+        /*
+        play audio
+        enable hover
+        */
+    }
+
+    public void SetIcon(Sprite newSprite)
+    {
+        icon = newSprite;
+    }
+
+    public void SetAudio(AudioClip clip)
+    {
+        audio = clip;
+    }
+
+    public void NavigateToPosition(Vector3 newPosition)
+    {
+        
+    }
 }
