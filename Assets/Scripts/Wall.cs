db363ffb76ca48942ec475674905241f85ae3443
Paulo Sergio da Silva Farias <psergio.farias@gmail.com>
2023-07-16 19:32:55 -0400

    Add Wall

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -5,7 +5,29 @@ public class Wall : MonoBehaviour
 {
     [SerializeField] private int collumns;
     [SerializeField] private int rows;
-    [SerializeField] private List<ButtonCandy> buttonList;
+    [SerializeField] private ButtonCandy buttonCandy;
+    [SerializeField] private Cell cell;
+    [SerializeField] private List<Cell> cellList;
 
-    
+
+    private void Start()
+    {   
+        PopulateWall();
+    }
+
+    private void PopulateWall()
+    {
+        for(int i = 0; i < rows; i++)
+            for(int j = 0; j < collumns; j++)
+            {
+                Cell NewCell = Instantiate(cell);
+                NewCell.SetPosition(new Vector3(i, j, 0));
+                cellList.Add(NewCell);
+            }
+    }
+
+    private void PutNewButtonIntoWall(Vector3 position, ButtonCandy button)
+    {
+        
+    }
 }
