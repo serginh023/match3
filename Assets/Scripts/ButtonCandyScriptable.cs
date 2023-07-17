bfcba79a0a80c95bedda78f7e60110676493a786
Paulo Sergio da Silva Farias <psergio.farias@gmail.com>
2023-07-17 17:46:29 -0400

    Ädd scriptables for jewels

---
+++ Assets/Scripts/ButtonCandyScriptable.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Button Candy", menuName = "New Button Candy", order = 1 )]
+public class ButtonCandyScriptable : ScriptableObject
+{
+    public Sprite icon;
+    public string name;
+}
